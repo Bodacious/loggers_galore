cfee14fe70a8af53507a28c327d07c888ce87a03
Gavin Morrice <gavin@thinkersplayground.com>
2009-06-22 11:35:53 +0100

    Fully tested

--- lib/loggers_galore.rb
+++ lib/loggers_galore.rb
@@ -3,17 +3,18 @@ module Rails
   class << self
     
     def extra_loggers= loggers
-      raise ArgumentError, 
-        "Params for loggers -#{loggers.inspect} doesn't look appropriate #{$@}" unless loggers.is_a?(Array) || loggers.is_a?(String) || loggers.is_a?(Symbol)
+      raise ArgumentError, "Params for loggers -#{loggers.inspect} don't look appropriate #{$@}" unless loggers.is_a?(Array)
       loggers.each do |logger|
+        raise ArgumentError unless logger.is_a?(String) || logger.is_a?(Symbol)
         logger = logger.to_s
         raise ArgumentError, "Can't call extra logger test, development or production... that would be confusing" if %w( development test production ).include?(logger)
         path = "#{Rails.root}/log/#{logger.downcase}.log"
         new_file = !File.exist?(path)
         new_logger = ActiveSupport::BufferedLogger.new path
         new_logger.info "" if new_file # => adds a line break if this is a new log
-        const = metaclass.const_set( "%s_LOGGER" % logger.upcase, new_logger )
-        Kernel.send( :define_method, :"#{logger.downcase}_logger" ) { const }
+        const = Object.const_set( "%s_LOGGER" % logger.upcase, new_logger )
+        # adds this method to Object - the mother of all classes!!!
+        Object.send(:define_method, :"#{logger.downcase}_logger") { const }
       end
     end
     
--- test/loggers_galore_test.rb
+++ test/loggers_galore_test.rb
@@ -1,8 +1,53 @@
 require 'test_helper'
 
 class LoggersGaloreTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def log_dir(filename)
+    "#{Rails.root}/log/%s.log" % filename.to_s
   end
+  
+  
+  test "should be able to set loggers through Rails module" do
+    assert Rails.respond_to?(:extra_loggers=)
+  end
+  
+  test "should raise an exception if arguements are not an array, string or symbol" do
+    assert_raise ArgumentError do
+      Rails.extra_loggers = [true, 1]
+    end
+  end
+  
+  test "should create a new log file in log dir" do
+    File.delete(log_dir :dummy) if File.exist?(log_dir :dummy)
+    Rails.extra_loggers = [:dummy]
+    assert File.exist?(log_dir :dummy)
+  end
+  
+  test "should raise an error if logger is called test, production or development" do
+    assert_raise ArgumentError do
+      Rails.extra_loggers = [:test]
+    end
+    assert_raise ArgumentError do
+      Rails.extra_loggers = [:production]
+    end
+    assert_raise ArgumentError do
+      Rails.extra_loggers = [:development]
+    end
+  end
+  
+  test "should add logger method to ActionController::Base, ActiveRecord::Base and ActionMailer::Base" do
+    Rails.extra_loggers = [ :alvin, :simon, :theodore ]
+    [ ActionController::Base, ActionMailer::Base, ActiveRecord::Base ].each do |fwrk|
+      assert fwrk.respond_to?("alvin_logger")
+      assert fwrk.respond_to?("simon_logger")
+      assert fwrk.respond_to?("theodore_logger")
+    end
+  end
+  
+  test "should add a line-break if this is a new log" do
+    File.delete(log_dir :another_dummy) if File.exist?(log_dir :another_dummy)
+    Rails.extra_loggers = [:another_dummy]
+    assert File.read(log_dir :another_dummy)["\n"]
+  end
+  
 end
